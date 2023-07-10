81919f0428be28d33824927e57fc9a60ed7a5a83
eunhye <yoonne0702@naver.com>
2023-07-10 22:19:19 +0900

    wirte AddPostView

--- front/src/views/AddPostView.vue
+++ front/src/views/AddPostView.vue
@@ -2,20 +2,33 @@
     <div>
         <h1>글등록</h1>
         <label>제목 :
-        <Input/></label>
+        <Input :value="title"/></label>
         <label>작성자 :
-        <Input/></label>
-        <label> :
-        <Input/></label>
-        <label>제목 :
-        <Input/></label>
-        <label>제목 :
-        <Input/></label>
+        <Input :value="author"/></label>
+        <label>작성일</label>
+        <input type="date" v-model="post.created_at">
+        <br/>
+        <label> 내용: </label>
+        <br/>
+        <textarea cols="25" rows="10" v-model="contents"></textarea>
+       
+        
     </div>
 </template>
 
 <script setup>
 import Input from '@/components/board/input.vue';
+import {ref,onMounted} from 'vue';
+
+const post = ref({
+    title:'', 
+    contents:'',
+    author:'', 
+    created_at:new Date().toISOString().substring(0, 10)
+})
+
+
+
  
 </script>
 
