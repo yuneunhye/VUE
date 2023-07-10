20b91a3eecbe1ce5104dc485633444d6ee890046
eunhye <yoonne0702@naver.com>
2023-07-10 17:57:26 +0900

    delete init  routers

---
+++ front/src/components/board/input.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+       <input type="text" />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
--- front/src/views/AddPostView.vue
+++ front/src/views/AddPostView.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <h1>글등록</h1>
+        <label>제목 :
+        <Input/></label>
+        <label>작성자 :
+        <Input/></label>
+        <label> :
+        <Input/></label>
+        <label>제목 :
+        <Input/></label>
+        <label>제목 :
+        <Input/></label>
     </div>
 </template>
 
 <script setup>
-
+import Input from '@/components/board/input.vue';
+ 
 </script>
 
 <style></style>
